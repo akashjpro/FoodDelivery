8424ba400f1730c483bf4fabf8b79af54fa437c1
akashjpro <akashjpro@gmail.com>
2021-04-23 13:51:13 +0700

    init project

---
+++ FoodDelivery/app/src/main/java/com/itsj/food/delivery/ui/loginandsignup/LoginAndSignupActivity.kt
@@ -0,0 +1,12 @@
+package com.itsj.food.delivery.ui.loginandsignup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.itsj.food.delivery.R
+
+class LoginAndSignupActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_login_and_signup)
+	}
+}
