6cc28b66308dfa1819f654913e01cf8c1b1cadf0
akashjpro <akashjpro@gmail.com>
2021-04-26 17:36:02 +0700

    Update ui & build.grandle

--- FoodDelivery/app/src/main/java/com/itsj/food/delivery/ui/splash/SplashActivity.kt
+++ FoodDelivery/app/src/main/java/com/itsj/food/delivery/ui/splash/SplashActivity.kt
@@ -1,12 +1,27 @@
 package com.itsj.food.delivery.ui.splash
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.itsj.food.delivery.R
+import com.itsj.food.delivery.ui.loginandsignup.LoginAndSignupActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_splash)
+
+		GlobalScope.launch(context = Dispatchers.Main) {
+			val intent = Intent(this@SplashActivity, LoginAndSignupActivity::class.java)
+			startActivity(intent)
+			finish()
+		}
+
 	}
+
+
+
 }
