c696fce01e99ce6e16b628708fc7e4ff774735fc
akashjpro <akashjpro@gmail.com>
2021-04-26 17:43:40 +0700

    Update activity

--- FoodDelivery/app/src/main/java/com/itsj/food/delivery/ui/loginandsignup/LoginAndSignUpActivity.kt
+++ FoodDelivery/app/src/main/java/com/itsj/food/delivery/ui/loginandsignup/LoginAndSignUpActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.itsj.food.delivery.R
 
-class LoginAndSignupActivity : AppCompatActivity() {
+class LoginAndSignUpActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_login_and_signup)
--- FoodDelivery/app/src/main/java/com/itsj/food/delivery/ui/splash/SplashActivity.kt
+++ FoodDelivery/app/src/main/java/com/itsj/food/delivery/ui/splash/SplashActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.itsj.food.delivery.R
-import com.itsj.food.delivery.ui.loginandsignup.LoginAndSignupActivity
+import com.itsj.food.delivery.ui.loginandsignup.LoginAndSignUpActivity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -15,7 +15,7 @@ class SplashActivity : AppCompatActivity() {
 		setContentView(R.layout.activity_splash)
 
 		GlobalScope.launch(context = Dispatchers.Main) {
-			val intent = Intent(this@SplashActivity, LoginAndSignupActivity::class.java)
+			val intent = Intent(this@SplashActivity, LoginAndSignUpActivity::class.java)
 			startActivity(intent)
 			finish()
 		}
