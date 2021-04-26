b54add4037e0bc4bb1cfcda044611335d9b6c5e8
DESKTOP-NUG1B8M\Bui Quang Tan Hy <tanhy411998@gmail.com>
2021-04-26 13:24:26 +0700

    UDATE UI

---
+++ FoodDelivery/app/src/main/java/com/itsj/food/delivery/ui/home/Home.kt
@@ -0,0 +1,12 @@
+package com.itsj.food.delivery.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.itsj.food.delivery.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home2)
+    }
+}
