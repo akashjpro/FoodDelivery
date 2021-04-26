bc6b7b60277c6da9e2d6e79bc1c50f7a63d8d57b
DESKTOP-NUG1B8M\Bui Quang Tan Hy <tanhy411998@gmail.com>
2021-04-26 14:27:22 +0700

    update ui

---
+++ FoodDelivery/app/src/main/java/com/itsj/food/delivery/ui/spicychiecrns/Spicy_chiecrns.kt
@@ -0,0 +1,12 @@
+package com.itsj.food.delivery.ui.spicychiecrns
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.itsj.food.delivery.R
+
+class Spicy_chiecrns : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spicy_chiecrns)
+    }
+}
