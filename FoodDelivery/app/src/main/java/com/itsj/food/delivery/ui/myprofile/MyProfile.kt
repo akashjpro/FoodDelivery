c1cabbcf7d2d59669ef6644124f7e55d81237fcc
akashjpro <akashjpro@gmail.com>
2021-04-27 15:26:04 +0700

    Refactor name

--- FoodDelivery/app/src/main/java/com/itsj/food/delivery/ui/myprofile/MyProfile.kt
+++ FoodDelivery/app/src/main/java/com/itsj/food/delivery/ui/myprofile/MyProfile.kt
@@ -1,10 +1,10 @@
-package com.itsj.food.delivery.ui.Myprofile
+package com.itsj.food.delivery.ui.myprofile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.itsj.food.delivery.R
 
-class Myprofile : AppCompatActivity() {
+class MyProfile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_myprofile)
