b60685e6b3bfcef3a6b16b4428b83a396b5f7825
Bui Quang Tan Hy <tanhy411998@gmail.com>
2021-04-27 10:58:04 +0700

    Fix ui

---
+++ FoodDelivery/app/src/main/java/com/itsj/food/delivery/ui/Myprofile/Myprofile.kt
@@ -0,0 +1,12 @@
+package com.itsj.food.delivery.ui.Myprofile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.itsj.food.delivery.R
+
+class Myprofile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_myprofile)
+    }
+}
