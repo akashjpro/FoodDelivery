1a9c4c983eddd8413a83306626cccd50ab14c498
Bui Quang Tan Hy <tanhy411998@gmail.com>
2021-04-28 17:28:21 +0700

    Update UI Checkout

---
+++ FoodDelivery/app/src/main/java/com/itsj/food/delivery/ui/checkout/Checkout.kt
@@ -0,0 +1,12 @@
+package com.itsj.food.delivery.ui.checkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.itsj.food.delivery.R
+
+class Checkout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkout)
+    }
+}
