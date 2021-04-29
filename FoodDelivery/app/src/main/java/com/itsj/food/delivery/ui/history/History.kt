2b5950f706158351ea8a096f8bfe73f41cf60275
Bui Quang Tan Hy <tanhy411998@gmail.com>
2021-04-29 11:47:07 +0700

    Update UI orders + history

--- FoodDelivery/app/src/main/java/com/itsj/food/delivery/ui/history/History.kt
+++ FoodDelivery/app/src/main/java/com/itsj/food/delivery/ui/history/History.kt
@@ -2,6 +2,7 @@ package com.itsj.food.delivery.ui.history
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.itsj.food.delivery.R
 
 class History : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- FoodDelivery/app/src/main/java/com/itsj/food/delivery/ui/orders/Orders.kt
+++ FoodDelivery/app/src/main/java/com/itsj/food/delivery/ui/orders/Orders.kt
@@ -2,6 +2,7 @@ package com.itsj.food.delivery.ui.orders
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.itsj.food.delivery.R
 
 class Orders : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
