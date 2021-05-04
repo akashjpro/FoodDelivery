df0c50e5cc195578dafeecf52a0f4bfff483e30d
Bui Quang Tan Hy <tanhy411998@gmail.com>
2021-05-04 14:44:10 +0700

    Update Ui

---
+++ FoodDelivery/app/src/main/java/com/its/food/delivery/ui/item_not_found/ItemNotFound.kt
@@ -0,0 +1,12 @@
+package com.its.food.delivery.ui.item_not_found
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.its.food.delivery.R
+
+class ItemNotFound : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_not_found)
+    }
+}
---
+++ FoodDelivery/app/src/main/java/com/its/food/delivery/ui/no_offers/NoOffers.kt
@@ -0,0 +1,12 @@
+package com.its.food.delivery.ui.no_offers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.its.food.delivery.R
+
+class NoOffers : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_no_offers)
+    }
+}
