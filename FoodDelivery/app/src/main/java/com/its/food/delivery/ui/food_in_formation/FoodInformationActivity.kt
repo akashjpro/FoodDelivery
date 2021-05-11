05cb793e9e267a7227324367181b0250feca9ba2
Bui Quang Tan Hy <tanhy411998@gmail.com>
2021-05-11 10:35:36 +0700

    update ui

--- FoodDelivery/app/src/main/java/com/its/food/delivery/ui/food_in_formation/FoodInformationActivity.kt
+++ FoodDelivery/app/src/main/java/com/its/food/delivery/ui/food_in_formation/FoodInformationActivity.kt
@@ -1,16 +1,21 @@
 package com.its.food.delivery.ui.food_in_formation
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.viewpager2.widget.ViewPager2
 import com.its.food.delivery.R
 import com.its.food.delivery.databinding.ActivityFoodInformationBinding
 import com.its.food.delivery.ui.BaseActivity
+import com.its.food.delivery.ui.checkout_payment.CheckOutPaymentActivity
+import com.its.food.delivery.ui.main.MainActivity
+import com.its.food.delivery.ui.main.home.HomeFragment
 import kotlinx.android.synthetic.main.activity_food_information.*
 import me.relex.circleindicator.CircleIndicator3
 
-class FoodInformationActivity : BaseActivity<ActivityFoodInformationBinding,FoodInformationViewModel>(){
-    private  var imagesList = mutableListOf<Int>()
+class FoodInformationActivity :
+    BaseActivity<ActivityFoodInformationBinding, FoodInformationViewModel>() {
+    private var imagesList = mutableListOf<Int>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,12 +24,13 @@ class FoodInformationActivity : BaseActivity<ActivityFoodInformationBinding,Food
         binding.viewModel = this.viewModel
 
         init()
-
+        observe()
         postToList()
 
         binding.viewpager2FoodInformation.adapter = SlideImageViewFood(imagesList)
         binding.viewpager2FoodInformation.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-        val indicator:CircleIndicator3 = findViewById<CircleIndicator3>(R.id.CircleIndicator3_Image_Food)
+        val indicator: CircleIndicator3 =
+            findViewById<CircleIndicator3>(R.id.CircleIndicator3_Image_Food)
         indicator.setViewPager(viewpager2FoodInformation)
     }
 
@@ -33,13 +39,17 @@ class FoodInformationActivity : BaseActivity<ActivityFoodInformationBinding,Food
         viewModel.processIntentData(intent)
     }
 
-    private fun addToList(image: Int){
+    private fun addToList(image: Int) {
         imagesList.add(image)
     }
 
-    private fun postToList(){
-        for (i in 1..5){
+    private fun postToList() {
+        for (i in 1..5) {
             addToList(R.drawable.img_image1)
         }
+    }
+
+    private fun observe() {
+
     }
 }
--- FoodDelivery/app/src/main/java/com/its/food/delivery/ui/food_in_formation/FoodInformationViewModel.kt
+++ FoodDelivery/app/src/main/java/com/its/food/delivery/ui/food_in_formation/FoodInformationViewModel.kt
@@ -8,8 +8,11 @@ import com.its.food.delivery.entity.Food
 import com.its.food.delivery.util.BUNDLE_KEY
 import com.its.food.delivery.util.FOOD_ENTITY_KEY
 import com.its.food.delivery.util.extensions.valueNotDistinct
+import com.its.food.delivery.util.livedata.SingleEvent
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.mapNotNull
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class FoodInformationViewModel @Inject constructor() : BaseViewModel() {
@@ -18,6 +21,11 @@ class FoodInformationViewModel @Inject constructor() : BaseViewModel() {
     val food by lazy {
         _food.filterNotNull().asLiveData(viewModelScope.coroutineContext)
     }
+//    private val _navigateToHome by lazy { MutableStateFlow<Boolean?>(null) }
+//    val navigateToHomeFragment by lazy {
+//        _navigateToHome.mapNotNull { SingleEvent.createOrNull(it) }
+//            .asLiveData(viewModelScope.coroutineContext)
+//    }
 
     fun processIntentData(intent: Intent?) {
         val food = intent?.getBundleExtra(BUNDLE_KEY)?.get(FOOD_ENTITY_KEY)  as Food
