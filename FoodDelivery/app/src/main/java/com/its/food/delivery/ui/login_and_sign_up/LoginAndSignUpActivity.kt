28ceada49ef93884fc50e69e0dc9952bcb738b6a
Bui Quang Tan Hy <tanhy411998@gmail.com>
2021-05-18 15:42:40 +0700

    Update error dialog

--- FoodDelivery/app/src/main/java/com/its/food/delivery/ui/login_and_sign_up/LoginAndSignUpActivity.kt
+++ FoodDelivery/app/src/main/java/com/its/food/delivery/ui/login_and_sign_up/LoginAndSignUpActivity.kt
@@ -1,9 +1,9 @@
 package com.its.food.delivery.ui.login_and_sign_up
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
@@ -13,6 +13,7 @@ import com.its.food.delivery.ui.BaseActivity
 import com.its.food.delivery.ui.main.MainActivity
 import com.its.food.delivery.util.EMPLOYEE
 import com.its.food.delivery.util.api.Resource
+import com.its.food.delivery.util.errorDialog
 import com.its.food.delivery.util.progressDialog
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -25,6 +26,7 @@ class LoginAndSignUpActivity :
     BaseActivity<ActivityLoginAndSignupBinding, LoginAndSignUpActivityViewModel>() {
 
     private var progressDialog: AlertDialog? = null
+    private var errorDialog: AlertDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -61,10 +63,11 @@ class LoginAndSignUpActivity :
         }
     }
 
+
     private fun processLogin(userName: String, password: String) {
 
         progressDialog = progressDialog(this, getString(R.string.logging_msg))
-
+        errorDialog = errorDialog(this,getString(R.string.error_title_error),getString(R.string.error_message),R.color.darkColor)
         viewModel.login(userName, password).observe(this) { resource ->
             when (resource) {
                 is Resource.Loading -> {
@@ -94,6 +97,7 @@ class LoginAndSignUpActivity :
 
                     } else {
                         // Show error dialog
+                        errorDialog?.dismiss()
                     }
                 }
             }
--- FoodDelivery/app/src/main/java/com/its/food/delivery/util/DialogUtil.kt
+++ FoodDelivery/app/src/main/java/com/its/food/delivery/util/DialogUtil.kt
@@ -1,13 +1,16 @@
 package com.its.food.delivery.util
 
 import android.app.Activity
+import android.app.Dialog
 import android.os.Build
 import android.view.View
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.its.food.delivery.R
+import kotlinx.android.synthetic.main.dialog_error.view.*
 import kotlinx.android.synthetic.main.dialog_progress.view.*
+import kotlin.system.exitProcess
 
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 fun progressDialog(
@@ -27,3 +30,32 @@ fun progressDialog(
             return this
         }
 }
+
+@RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+fun errorDialog(
+    context: Activity,
+    message: String,
+    title: String,
+    color: Int
+): AlertDialog {
+    val contentView: View = context.layoutInflater.inflate(R.layout.dialog_error, null)
+    contentView.tvTitleError.text = title
+    contentView.tvMessageError.text = message
+    contentView.backgroundDialogError.setCardBackgroundColor(color)
+    contentView.btnErrorOk.setOnClickListener{
+        Dialog(context).cancel()
+    }
+    MaterialAlertDialogBuilder(context)
+        .setView(contentView)
+        .create()
+        .apply {
+            setCanceledOnTouchOutside(false)
+            setCancelable(false)
+            window?.setBackgroundDrawable(context.getDrawable(R.drawable.background_dialog))
+            return this
+        }
+
+
+}
+
+
